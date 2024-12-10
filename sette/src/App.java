6388dd2cb6a92cabc0434b4f4e10d41a37ff4a17
Di Giacomo Daniel <daniel.digiacomo98@gmail.com>
2024-12-10 10:15:15 +0100

    passo 2 fatto

--- sette/src/App.java
+++ sette/src/App.java
@@ -3,40 +3,60 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc=new Scanner(System.in);
-        Random r=new Random();
 
+        // Dichiariamo lo scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Dichiariamo il random
+        Random r = new Random();
+
+        // Dichiariamo le variabili
         double carta, punteggio;
         boolean risposta;
+        String seme;
 
-        risposta=true;
-        punteggio=0;
+        // Dichiaro array e lo popolo
+        String[] semi = { "denari", "bastoni", "coppe", "spade" };
 
-        while (risposta) { carta= r.nextInt(10)+1;
-            System.out.println("La carta estratta è: " +carta);
+        // inizializziamo le variabili
+        risposta = true;
+        punteggio = 0;
 
-            if (carta>=8) {
-                carta=0.5;   
+        // Ciclo while che controlla il risultato e se vuole giocare ancora
+        while (risposta) {
+
+            // Creiamo un numero casuale da 1 a 10
+            carta = r.nextInt(10) + 1;
+
+            // Creiamo un seme casuale
+            seme = semi[r.nextInt(4)];
+
+            // Esponiamo il risultato della carta
+            System.out.println("La carta estratta è: " + carta + " di " + seme);
+
+            // Se maggiore o uguale a 8 punteggio 0.5
+            if (carta >= 8) {
+                carta = 0.5;
             }
 
-            punteggio=punteggio+carta;
-            System.out.println("Il tuo punteggio è : "+punteggio);
+            // Sommo i punti
+            punteggio = punteggio + carta;
+            System.out.println("Il tuo punteggio è : " + punteggio);
 
-            if (punteggio>7.5) {
+            // Controllo il risultato
+            if (punteggio > 7.5) {
                 System.out.println("Hai sballato!");
-                risposta=false;
-            } else if (punteggio==7.5) {
+                risposta = false;
+            } else if (punteggio == 7.5) {
                 System.out.println("Hai vinto!");
-                risposta=false;
-            } else          
-            { System.out.println("Vuoi estrarre un'altra carta?");
-            risposta= sc.nextBoolean(); }
-            
+                risposta = false;
+            } else {
+                System.out.println("Vuoi estrarre un'altra carta?");
+                risposta = sc.nextBoolean();
+            }
         }
 
         sc.close();
-
-
 
     }
 }
