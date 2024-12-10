766e53d40cdece72670779b46372a097861795b1
Dominic <118680299+bjdome@users.noreply.github.com>
2024-12-10 11:29:13 +0100

    passo 3

--- sette/src/App.java
+++ sette/src/App.java
@@ -1,7 +1,9 @@
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
 
         // Dichiariamo lo scanner
@@ -11,15 +13,16 @@ public class App {
         Random r = new Random();
 
         // Dichiariamo le variabili
-        double carta, punteggio;
-        boolean risposta;
+        double punteggio;
+        boolean risposta=true, uscita=true;
         String seme;
+        int i, carta;
 
         // Dichiaro array e lo popolo
-        String[] semi = { "denari", "bastoni", "coppe", "spade" };
+        String[] semi = {"denari", "bastoni", "coppe", "spade"};
+        boolean[] carteUscite = new boolean[40];
 
         // inizializziamo le variabili
-        risposta = true;
         punteggio = 0;
 
         // Ciclo while che controlla il risultato e se vuole giocare ancora
@@ -31,16 +34,63 @@ public class App {
             // Creiamo un seme casuale
             seme = semi[r.nextInt(4)];
 
+            // Controllo carta uscita
+            while(uscita){
+                i=0;
+                switch(seme){
+                    case "denari":
+                        i=carta-1;
+                        if(carteUscite[i]==true){
+                            carta = r.nextInt(10) + 1;
+                            seme = semi[r.nextInt(4)];
+                        }else{
+                            carteUscite[i]=true;
+                            uscita=false;
+                        }
+                        break;
+                    case "bastoni":
+                    i=9+carta;
+                    if(carteUscite[i]==true){
+                        carta = r.nextInt(10) + 1;
+                        seme = semi[r.nextInt(4)];
+                    }else{
+                        carteUscite[i]=true;
+                        uscita=false;
+                    }
+                    break;
+                    case "coppe":
+                    i=19+carta;
+                    if(carteUscite[i]==true){
+                        carta = r.nextInt(10) + 1;
+                        seme = semi[r.nextInt(4)];
+                    }else{
+                        carteUscite[i]=true;
+                        uscita=false;
+                    }
+                    break;
+                    case "spade":
+                    i=29+carta;
+                    if(carteUscite[i]==true){
+                        carta = r.nextInt(10) + 1;
+                        seme = semi[r.nextInt(4)];
+                    }else{
+                        carteUscite[i]=true;
+                        uscita=false;
+                    }
+                    break;
+                }
+            }
+            uscita=true;
+
             // Esponiamo il risultato della carta
             System.out.println("La carta estratta è: " + carta + " di " + seme);
 
             // Se maggiore o uguale a 8 punteggio 0.5
             if (carta >= 8) {
-                carta = 0.5;
+                punteggio += 0.5;
+            }else{
+                punteggio+=carta;
             }
-
-            // Sommo i punti
-            punteggio = punteggio + carta;
             System.out.println("Il tuo punteggio è : " + punteggio);
 
             // Controllo il risultato
@@ -54,6 +104,7 @@ public class App {
                 System.out.println("Vuoi estrarre un'altra carta?");
                 risposta = sc.nextBoolean();
             }
+            
         }
 
         sc.close();
