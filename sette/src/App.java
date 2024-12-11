67426b4e8bf8e6ef2d4679f0eab2d91cad4231b0
Alisse95 <alikea.info@gmail.com>
2024-12-11 10:33:31 +0100

    punto 5 update

--- sette/src/App.java
+++ sette/src/App.java
@@ -13,217 +13,221 @@ public class App {
         Random r = new Random();
 
         // Dichiariamo le variabili
-        double punteggio,punteggioBanco=0;
-        boolean risposta=true, uscita=true;
-        String seme,semeBanco;
-        int i, carta,puntata,budget,cartaBanco;
-        budget=100;
+        double punteggio, punteggioBanco = 0;
+        boolean risposta = true, uscita = true, risposta_sballo = true;
+        String seme, semeBanco;
+        int i, carta, puntata, budget, cartaBanco;
+        budget = 100;
 
-        
         do {
-            System.out.println("Inserisci la puntata: ");
-            puntata= sc.nextInt();
-        if (puntata>budget || budget==0) {
-            System.out.println("Sei iper povero, ricarica i soldi");
-    
-        } else if (puntata<=0) {
-            System.out.println("Scommessa non valida");
-        }
-        else
-        System.out.println("Hai giocato: " + puntata + " Euro");
-        } while (puntata> budget || budget==0 || puntata<=0);
-
-
-
-
-
-        // Dichiaro array e lo popolo
-        String[] semi = {"denari", "bastoni", "coppe", "spade"};
-        boolean[] carteUscite = new boolean[40];
-        boolean[] carteUsciteBanco = new boolean[40];
-
-        // inizializziamo le variabili
-        punteggio = 0;
-
-        // Ciclo while che controlla il risultato e se vuole giocare ancora
-        while (risposta) {
-
-            // Creiamo un numero casuale da 1 a 10
-            carta = r.nextInt(10) + 1;
             
+            do {
+                System.out.println("Inserisci la puntata: ");
+                puntata = sc.nextInt();
+                if (puntata > budget || budget == 0) {
+                    System.out.println("Sei iper povero, ricarica i soldi");
+                    System.out.println("I soldi a disposizione sono: "+ budget);
 
-            // Creiamo un seme casuale
-            seme = semi[r.nextInt(4)];
-            
-
-            // Controllo carta uscita
-            while(uscita){
-                i=0;
-                switch(seme){
-                    case "denari":
-                        i=carta-1;
-                        if(carteUscite[i]==true){
-                            carta = r.nextInt(10) + 1;
-                            seme = semi[r.nextInt(4)];
-                        }else{
-                            carteUscite[i]=true;
-                            uscita=false;
-                        }
-                        break;
-                    case "bastoni":
-                    i=9+carta;
-                    if(carteUscite[i]==true){
-                        carta = r.nextInt(10) + 1;
-                        seme = semi[r.nextInt(4)];
-                    }else{
-                        carteUscite[i]=true;
-                        uscita=false;
-                    }
-                    break;
-                    case "coppe":
-                    i=19+carta;
-                    if(carteUscite[i]==true){
-                        carta = r.nextInt(10) + 1;
-                        seme = semi[r.nextInt(4)];
-                    }else{
-                        carteUscite[i]=true;
-                        uscita=false;
-                    }
-                    break;
-                    case "spade":
-                    i=29+carta;
-                    if(carteUscite[i]==true){
-                        carta = r.nextInt(10) + 1;
-                        seme = semi[r.nextInt(4)];
-                    }else{
-                        carteUscite[i]=true;
-                        uscita=false;
-                    }
-                    break;
+                } else if (puntata <= 0) {
+                    System.out.println("Scommessa non valida");
+                } else {
+                    System.out.println("Hai giocato: " + puntata + " Euro");
                 }
-            }
-            uscita=true;
-    
-            
+            } while (puntata > budget || budget == 0 || puntata <= 0);
 
-            // Esponiamo il risultato della carta
-            System.out.println("La carta estratta è: " + carta + " di " + seme);
-            if (carta >= 8) {
-                punteggio += 0.5;
-            }else{
-                punteggio+=carta;
-            }
-            System.out.println("Il tuo punteggio è : " + punteggio);
-            System.out.println("Vuoi estrarre un'altra carta?");
-            risposta = sc.nextBoolean();
-            
+            // Dichiaro array e lo popolo
+            String[] semi = {"denari", "bastoni", "coppe", "spade"};
+            boolean[] carteUscite = new boolean[40];
+            boolean[] carteUsciteBanco = new boolean[40];
 
-            
-            // Se maggiore o uguale a 8 punteggi
-            
-        
-            if (punteggio > 7.5) {
-                System.out.println("Hai sballato!");
-                budget= budget-puntata;
-                risposta = false;
-            } 
-        
+            // inizializziamo le variabili
+            punteggio = 0;
 
-        }
+            // Ciclo while che controlla il risultato e se vuole giocare ancora
+            while (risposta) {
 
-        while (risposta) {
+                // Creiamo un numero casuale da 1 a 10
+                carta = r.nextInt(10) + 1;
 
-            cartaBanco= r.nextInt(10) + 1;
-            semeBanco = semi[r.nextInt(4)];
+                // Creiamo un seme casuale
+                seme = semi[r.nextInt(4)];
 
-            while(uscita){
-                i=0;
-                switch(semeBanco){
-                    case "denari":
-                        i=cartaBanco-1;
-                        if(carteUsciteBanco[i]==true){
-                            cartaBanco = r.nextInt(10) + 1;
-                            semeBanco = semi[r.nextInt(4)];
-                        }else{
-                            carteUsciteBanco[i]=true;
-                            uscita=false;
-                        }
-                        break;
-                    case "bastoni":
-                    i=9+cartaBanco;
-                    if(carteUsciteBanco[i]==true){
-                        cartaBanco = r.nextInt(10) + 1;
-                        semeBanco = semi[r.nextInt(4)];
-                    }else{
-                        carteUsciteBanco[i]=true;
-                        uscita=false;
+                // Controllo carta uscita
+                while (uscita) {
+                    i = 0;
+                    switch (seme) {
+                        case "denari":
+                            i = carta - 1;
+                            if (carteUscite[i] == true) {
+                                carta = r.nextInt(10) + 1;
+                                seme = semi[r.nextInt(4)];
+                            } else {
+                                carteUscite[i] = true;
+                                uscita = false;
+                            }
+                            break;
+                        case "bastoni":
+                            i = 9 + carta;
+                            if (carteUscite[i] == true) {
+                                carta = r.nextInt(10) + 1;
+                                seme = semi[r.nextInt(4)];
+                            } else {
+                                carteUscite[i] = true;
+                                uscita = false;
+                            }
+                            break;
+                        case "coppe":
+                            i = 19 + carta;
+                            if (carteUscite[i] == true) {
+                                carta = r.nextInt(10) + 1;
+                                seme = semi[r.nextInt(4)];
+                            } else {
+                                carteUscite[i] = true;
+                                uscita = false;
+                            }
+                            break;
+                        case "spade":
+                            i = 29 + carta;
+                            if (carteUscite[i] == true) {
+                                carta = r.nextInt(10) + 1;
+                                seme = semi[r.nextInt(4)];
+                            } else {
+                                carteUscite[i] = true;
+                                uscita = false;
+                            }
+                            break;
                     }
-                    break;
-                    case "coppe":
-                    i=19+cartaBanco;
-                    if(carteUsciteBanco[i]==true){
-                        cartaBanco = r.nextInt(10) + 1;
-                        semeBanco = semi[r.nextInt(4)];
-                    }else{
-                        carteUsciteBanco[i]=true;
-                        uscita=false;
-                    }
-                    break;
-                    case "spade":
-                    i=29+cartaBanco;
-                    if(carteUsciteBanco[i]==true){
-                        cartaBanco = r.nextInt(10) + 1;
-                        semeBanco = semi[r.nextInt(4)];
-                    }else{
-                        carteUsciteBanco[i]=true;
-                        uscita=false;
-                    }
-                    break;
                 }
+                uscita = true;
+
+                // Esponiamo il risultato della carta
+                System.out.println("La carta estratta è: " + carta + " di " + seme);
+                if (carta >= 8) {
+                    punteggio += 0.5;
+                } else {
+                    punteggio += carta;
+                }
+                System.out.println("Il tuo punteggio è : " + punteggio);
+
+                // Se maggiore o uguale a 8 punteggi
+                if (punteggio > 7.5) {
+                    System.out.println("Hai sballato!"); // se sballiamo il gioco finisce
+                    budget = budget - puntata;
+                    System.out.println("Il tuo bugdet attuale: "+ budget);
+                    risposta = false;
+                    System.out.println("Vuoi giocare ancora?!");
+                    risposta_sballo = sc.nextBoolean();
+                } else {
+                    System.out.println("Vuoi estrarre un'altra carta?!");
+                    risposta = sc.nextBoolean();
+                }
+                
+
             }
-            uscita=true;
+            risposta = true;
 
+            if (punteggio <= 7.5) {
+                while (punteggioBanco <= 7.5 && punteggioBanco <= punteggio) {
+                    cartaBanco = r.nextInt(10) + 1;
+                    semeBanco = semi[r.nextInt(4)];
 
+                    while (uscita) {
+                        i = 0;
+                        switch (semeBanco) {
+                            case "denari":
+                                i = cartaBanco - 1;
+                                if (carteUsciteBanco[i] == true) {
+                                    cartaBanco = r.nextInt(10) + 1;
+                                    semeBanco = semi[r.nextInt(4)];
+                                } else {
+                                    carteUsciteBanco[i] = true;
+                                    uscita = false;
+                                }
+                                break;
+                            case "bastoni":
+                                i = 9 + cartaBanco;
+                                if (carteUsciteBanco[i] == true) {
+                                    cartaBanco = r.nextInt(10) + 1;
+                                    semeBanco = semi[r.nextInt(4)];
+                                } else {
+                                    carteUsciteBanco[i] = true;
+                                    uscita = false;
+                                }
+                                break;
+                            case "coppe":
+                                i = 19 + cartaBanco;
+                                if (carteUsciteBanco[i] == true) {
+                                    cartaBanco = r.nextInt(10) + 1;
+                                    semeBanco = semi[r.nextInt(4)];
+                                } else {
+                                    carteUsciteBanco[i] = true;
+                                    uscita = false;
+                                }
+                                break;
+                            case "spade":
+                                i = 29 + cartaBanco;
+                                if (carteUsciteBanco[i] == true) {
+                                    cartaBanco = r.nextInt(10) + 1;
+                                    semeBanco = semi[r.nextInt(4)];
+                                } else {
+                                    carteUsciteBanco[i] = true;
+                                    uscita = false;
+                                }
+                                break;
+                        }
+                    }
+                    uscita = true;
 
-            System.out.println("La carta estratta dal banco è: " + cartaBanco + " di " + semeBanco);
+                    System.out.println("La carta estratta dal banco è: " + cartaBanco + " di " + semeBanco);
 
-            if (cartaBanco >= 8) {
-                punteggioBanco += 0.5;
-            }else{
-                punteggioBanco+=cartaBanco;
-            }
-            
-            System.out.println("Il punteggio del Banco è : " + punteggioBanco);
+                    if (cartaBanco >= 8) {
+                        punteggioBanco += 0.5;
+                    } else {
+                        punteggioBanco += cartaBanco;
+                    }
 
+                    System.out.println("Il punteggio del Banco è : " + punteggioBanco);
 
+                }
+                // finel del gioco e andiamo a calcolare il punteggio del tutto
+                if (punteggio > 7.5) {
+                    // L'utente ha sballato
+                    System.out.println("Hai sballato!");
+                    budget = budget - puntata;
+                    System.out.println("Budget rimanente = " + budget);
+                    risposta = false;
+                } else if (punteggio == 7.5 || punteggioBanco > 7.5) {
+                    System.out.println("L'utente ha vinto!");
+                    budget = budget + puntata;
+                    System.out.println("Budget rimanente = " + budget);
+                    risposta = false;
+                } else if (punteggio == punteggioBanco) {
 
+                    System.out.println("Il banco ha vinto!");
+                    budget = budget - puntata;
+                    System.out.println("Budget rimanente = " + budget);
+                    risposta = false;
+                } else if (punteggioBanco > punteggio && punteggioBanco <= 7.5) {
 
+                    System.out.println("Il banco ha vinto!");
+                    budget = budget - puntata;
+                    System.out.println("Budget rimanente = " + budget);
+                    risposta = false;
+                } else {
 
+                    System.out.println("Budget rimanente = " + budget);
+                    System.out.println("Vuoi estrarre un'altra carta?");
+                    risposta = sc.nextBoolean();
+
+                }
+                System.out.println("Vuoi giocare ancora?!");
+                risposta_sballo = sc.nextBoolean();
+            } }
+            while (risposta_sballo);
+            System.out.println("Grazie per aver giocato, sei diventato troppo povero vai a lavorare di nuovo!!!!");
+
+            sc.close();
         }
 
-        if (punteggio > 7.5) {
-            System.out.println("Hai sballato!");
-            budget= budget-puntata;
-            risposta = false;
-        } 
-        
-        else if(punteggio == 7.5 || punteggioBanco>7.5) {
-            System.out.println("L'utente ha vinto!");
-            budget= budget+puntata;
-            
-            risposta = false;
-        } else if (punteggio==punteggioBanco) {
-            System.out.println("Il banco ha vinto!");
-            budget= budget-puntata;
-            risposta = false;
-        }
-        else {
-            System.out.println("Budget rimanente = " + budget);
-            System.out.println("Vuoi estrarre un'altra carta?");
-            risposta = sc.nextBoolean();
-        } 
-
-        sc.close();
 
     }
-}
